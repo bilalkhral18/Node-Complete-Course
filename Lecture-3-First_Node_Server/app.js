5d485352cdfd1df79a072f9770b513ca11e75ce0
Your Name <tech.com88905@gmail.com>
2025-12-21 22:19:13 +0500

    14:19 node.js 4th lecture remains

--- Lecture-3-First_Node_Server/app.js
+++ Lecture-3-First_Node_Server/app.js
@@ -1,13 +1,12 @@
-// Core Modules
+//   CORE MODULE
 const http = require("http");
 
-// Now I have to create a http server
-const server = http.createServer((req, res) => {
+const SERVER = http.createServer((req, res) => {
   console.log(req);
 });
 
 const PORT = 3300;
-// Now Server is started listening on a PORT 3300
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+
+SERVER.listen(PORT, () => {
+  console.log(`Server Listen On a ${PORT}`);
 });
--- Lecture-4-Request_and_Response/app.js
+++ Lecture-4-Request_and_Response/app.js
@@ -1,58 +1,12 @@
+// CORE MODULE
 const http = require("http");
-const fs = require("fs");
 
-const Server = http.createServer((req, res) => {
+const SERVER = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
-  const url = req.url.toLowerCase();
-  // process.exit(); //Stops Event Loop
-  // res.setHeader("Content-Type", "text/html");
-  // if (url === "/") {
-  //   res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  //   return res.end();
-  // } else if (url === "/products") {
-  //   res.write("<h4>This is the products page</h4>");
-  //   return res.end();
-  // } else {
-  //   res.write("<h1>404 Error Page</h1>");
-  //   return res.end();
-  // }
-  if (req.url === "/") {
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<h1>Enter Your Details</h1>");
-    res.write("<form action='/create-user' method='POST'>");
-    res.write("<label for='username'>Name:</label>");
-    res.write(
-      "<input type='text' id='username' name='username' placeholder='Enter your name' required>"
-    );
-    res.write("<label for='Gender'>Gender:</label>");
-    res.write("<input type='radio' id='male' name='gender' value='male'>");
-    res.write("<label for='male'>Male:</label> </br>");
-
-    res.write("<input type='radio' id='female' name='gender' value='female'>");
-    res.write("<label for='female'>Female:</label> </br>");
-
-    res.write("<input type='radio' id='other' name='gender' value='other'>");
-    res.write("<label for='other'>Other:</label> </br>");
-
-    res.write("<button type='submit'>Submit</button>");
-    res.write("</form>");
-    res.write("</html>");
-    return res.end(); // Ends the response
-  } else if (url === "/create-user" && req.method === "POST") {
-    fs.writeFileSync("user.txt", "Bilal Rai");
-    res.statusCode = 302; // Redirects to the home page
-    res.setHeader("Location", "/");
-    return res.end(); // Ends the response
-  } else {
-    res.write("<h1>404 Error Page</h1>");
-    res.write("</html>");
-    return res.end();
-  }
-  // return res.end(); // Ends the response
+  // process.exit(); This is used to exit the event loop
 });
 
-const PORT = 3000;
-Server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const PORT = 3200;
+SERVER.listen(PORT, () => {
+  console.log(`server listen on  http://localhost:${PORT}`);
 });
